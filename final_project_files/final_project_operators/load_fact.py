2c6fb49b3a88f8c0fa9fbd098ec6ee8aa0f170fc
Akansha Singh <akanshasingh1202@gmail.com>
2024-09-07 09:34:07 +0200

    restructured the project

---
+++ final_project_files/final_project_operators/load_fact.py
@@ -0,0 +1,36 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from udacity.common import final_project_sql_statements
+
+class LoadFactOperator(BaseOperator):
+
+    ui_color = '#F98866'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 create_table_sql=None,
+                 sql_query="",
+                 append_data=False,
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.create_table_sql = create_table_sql
+        self.sql_query = sql_query
+        self.append_data = append_data
+
+    def execute(self, context):
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.create_table_sql:
+            self.log.info(f"Creating table {self.table} if it doesn't exist")
+            redshift_hook.run(self.create_table_sql)
+
+        # Insert data into the table
+        self.log.info(f"Inserting data into fact table {self.table}")
+        redshift_hook.run(self.sql_query)
+        self.log.info(f"Fact table {self.table} loaded successfully.")
